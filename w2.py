93421a267e763a8e8a31483ec1ab95e2292705b9
shiyujiucsb <shiyu@cs.ucsb.edu>
2017-01-15 02:24:20 -0800

    3

--- w2.py
+++ w2.py
@@ -18,7 +18,53 @@ for i in xrange(n):
     X_i = X_i.tolist()
     X[i] = X_i
 
-	
+nBuckets = 20
+nDim = len(X[0][0])
+import random as r
+r.seed(10)
+LSH = []
+for i in range(nBuckets):
+    vec = []
+    for j in range(nDim):
+        vec.append(r.uniform(-1,1))
+    LSH.append(vec)
+
+def dot(v, u):
+    res = 0.0
+    for i in range(len(v)):
+        res += v[i] * u[i]
+    return res
+
+def sign(V):
+    sig = ""
+    for i in range(len(LSH)):
+        if dot(V, LSH[i]) > 0:
+            sig+="1"
+        else:
+            sig+="0"
+    return int(sig, 2)
+
+def helper(X1, BOW1, X2, BOW2):
+    slots1 = {}
+    slots2 = {}
+    for i in range(len(X1)):
+        s = sign(X1[i])
+        if s in slots1:
+            slots1[s] += BOW1[i]
+        else:
+            slots1[s] = BOW1[i]
+    for i in range(len(X2)):
+        s = sign(X2[i])
+        if s in slots2:
+            slots2[s] += BOW2[i]
+        else:
+            slots2[s] = BOW2[i]
+    res = 0.0
+    for k in slots1.keys():
+        if k in slots2:
+            res += slots1[k] * slots2[k]
+    return res
+
 def get_wmd(ix):
     n = np.shape(X)
     n = n[0]
@@ -27,10 +73,11 @@ def get_wmd(ix):
     #print '%d out of %d' % (i, n)
     for j in xrange(i):
         #Di[0,j] = emd( (X[i], BOW_X[i]), (X[j], BOW_X[j]), distance)
-		print(X[i])
-		print(BOW_X[i])
-    return Di 
-            
+        Di[0,j] = helper(X[i], BOW_X[i], X[j], BOW_X[j])
+        #if Di[0,j]>0.4 and min(len(X[i]), len(X[j])) >10:
+        print(ix," and ",j,": ", Di[0, j])
+    return Di
+
 
 def main():
     n = np.shape(X)
@@ -50,7 +97,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
